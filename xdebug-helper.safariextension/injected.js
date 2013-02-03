8cdfcaffeb841afc97bb2776a422ef10384fd80c
Mathijs Kadijk <mathijs@wrep.nl>
2013-02-03 17:20:50 +0100

    Added Cookie methods and a dummy setState method/handler

--- xdebug-helper.safariextension/injected.js
+++ xdebug-helper.safariextension/injected.js
@@ -2,16 +2,61 @@ var xdebug = (function() {
 	var exposed = {
 			// Handle incomming messages
 			onMessage : function(event) {
-				if (event.name == "getState") {
-					safari.self.tab.dispatchMessage("getStateResponse", getState());
+				if ("getState" == event.name) {
+					safari.self.tab.dispatchMessage("getStateResponse", getState(event.message.ideKey));
+				} else if ("setState" == event.name) {
+					setState(event.message.ideKey, event.message.state);
 				} else {
-					alert(event.name + ": " + event.message);
+					console.log("[Xdebug helper] Injected script received unknown message: " + event.name);
 				}
 			}
 		};
 
-	function getState() {
+	function getState(ideKey) {
 		return "debugging";
+	}
+
+	function setState(ideKey, newState) {
+		alert("setState(" + ideKey + ", " + newState + ")");
+	}
+
+	// Set a cookie
+	function setCookie(name, value, hours)
+	{
+		var exp = new Date();
+		exp.setTime(exp.getTime() + (hours * 60 * 60 * 1000));
+		document.cookie = name + "=" + value + "; expires=" + exp.toGMTString() + "; path=/";
+	}
+
+	// Get the content in a cookie
+	function getCookie(name)
+	{
+		// Search for the start of the goven cookie
+		var prefix = name + "=",
+			cookieStartIndex = document.cookie.indexOf(prefix),
+			cookieEndIndex;
+
+		// If the cookie is not found return null
+		if (cookieStartIndex == -1)
+		{
+			return null;
+		}
+
+		// Look for the end of the cookie
+		cookieEndIndex = document.cookie.indexOf(";", cookieStartIndex + prefix.length);
+		if (cookieEndIndex == -1)
+		{
+			cookieEndIndex = document.cookie.length;
+		}
+
+		// Extract the cookie content
+		return unescape(document.cookie.substring(cookieStartIndex + prefix.length, cookieEndIndex));
+	}
+
+	// Remove a cookie
+	function deleteCookie(name)
+	{
+		setCookie(name, null, -60);
 	}
 
 	return exposed;
